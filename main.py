bab00e87e1f49194aade0a7c8388ae4bee87b63f
prem <prem85chandra@gmail.com>
2020-08-24 23:23:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def m1():
+	print("Hello Word- m1")
+	m2()
+
+def m2():
+	print("Hello Word- m2")
+	m3()
+	
+def m3():
+	print("Hello Word- m3")
+	m4()
+	
+def m4():
+	print("Hello Word- m4")
+
+
+if __name__ == '__main__':
+	m1()
